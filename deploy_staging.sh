78e2f014fcb50b8d8aa17b71e5a5b30d7d35dbcf
dreboard <dre.board@gmail.com>
2017-08-18 02:00:57 -0400

    staging shell files

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
 # Deploy staging branch
-
+# git pull origin staging
 echo ".....preparing commands"
 vagrant ssh -c "
-cd  /var/www/staging
-if [ -d .git ]; then
-  git init
-  git remote add origin https://github.com/dreboard/multi_env.git
-else
-  echo no git installed
-fi;
-git pull origin staging
 
+if [ -d .git ]; then
+  echo ......initialize git;
+  env -i git init
+  env -i git remote add origin https://github.com/dreboard/multi_env.git
+
+else
+  echo ......pulling staging branch;
+  env -i git pull origin master
+fi;
+
+ls -ah  /var/www/staging
 "
